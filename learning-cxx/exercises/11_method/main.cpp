e5e9aa86a43a1be34af9e219da1778eaa4e8ae7d
superme1one <58428737+superme1one@users.noreply.github.com>
2025-07-24 16:59:12 +0800

    ./
    first

---
+++ learning-cxx/exercises/11_method/main.cpp
@@ -0,0 +1,27 @@
+#include "../exercise.h"
+
+struct Fibonacci {
+    unsigned long long cache[128];
+    int cached;
+
+    // TODO: 实现正确的缓存优化斐波那契计算
+    unsigned long long get(int i) {
+        if (i <= cached) {
+            return cache[i];
+        }
+        for (int j = cached + 1; j <= i; ++j) {
+            cache[j] = cache[j - 1] + cache[j - 2];
+        }
+
+        cached = i; 
+        return cache[i];
+    }
+};
+
+int main(int argc, char **argv) {
+    // TODO: 初始化缓存结构体，使计算正确
+    Fibonacci fib{{0,1},1};
+    ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
+    std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
+    return 0;
+}
---
+++ learning-cxx/exercises/20_function_template/main.cpp
@@ -0,0 +1,29 @@
+﻿#include "../exercise.h"
+
+// READ: 函数模板 <https://zh.cppreference.com/w/cpp/language/function_template>
+// TODO: 将这个函数模板化
+#include <cmath>
+template<typename T>
+
+T plus(T a, T b) {
+    return a + b;
+}
+
+int main(int argc, char **argv) {
+    ASSERT(plus(1, 2) == 3, "Plus two int");
+    ASSERT(plus(1u, 2u) == 3u, "Plus two unsigned int");
+
+    // THINK: 浮点数何时可以判断 ==？何时必须判断差值？
+    ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
+    ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
+    // TODO: 修改判断条件使测试通过
+    const double result = plus(0.1, 0.2);
+    const double expected = 0.3;
+    const double eps = 1e-12;
+
+    ASSERT(std::fabs(result - expected) < eps, "How to make this pass?");
+
+
+
+    return 0;
+}
